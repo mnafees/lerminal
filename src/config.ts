d2738f2b3f268f5b8606d3802b316d760c94ce7c
Mohammed Nafees <hello@mnafees.me>
2019-03-13 00:00:48 -0400

    coding guidelines for config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,50 +4,50 @@ import * as fs from 'fs';
 
 export class LerminalConfig {
 
-  private static instance : LerminalConfig;
-  private filePath : string = `${os.homedir()}/.config/lerminal.json`;
-  private contents = Object();
+  private static s_instance : LerminalConfig;
+  private _filePath : string = `${os.homedir()}/.config/lerminal.json`;
+  private _contents = Object();
 
   private constructor() {
     this.readContents();
   }
 
-  static getInstance() : LerminalConfig {
-    if (!LerminalConfig.instance) {
-      LerminalConfig.instance = new LerminalConfig();
+  static get instance() : LerminalConfig {
+    if (!LerminalConfig.s_instance) {
+      LerminalConfig.s_instance = new LerminalConfig();
     }
 
-    return LerminalConfig.instance;
+    return LerminalConfig.s_instance;
   }
 
   set configFilePath(customFilePath : string) {
-    this.filePath = customFilePath;
+    this._filePath = customFilePath;
     this.readContents();
   }
 
   get configFilePath() : string {
-    return this.filePath;
+    return this._filePath;
   }
 
-  getValue(key?: string) : any {
-    if (key) return this.contents[key];
+  value(key?: string) : any {
+    if (key) return this._contents[key];
 
-    return this.contents;
+    return this._contents;
   }
 
   setValue(key: string, val: any) {
-    this.contents[key] = val;
+    this._contents[key] = val;
   }
 
   private readContents() {
-    if (fs.existsSync(this.filePath)) {
-      const data = fs.readFileSync(this.filePath);
-      this.contents = data.toString();
+    if (fs.existsSync(this._filePath)) {
+      const data = fs.readFileSync(this._filePath);
+      this._contents = data.toString();
     }
   }
 
   save() {
-    fs.writeFileSync(this.filePath, JSON.stringify(this.contents));
+    fs.writeFileSync(this._filePath, JSON.stringify(this._contents));
   }
 
 }
